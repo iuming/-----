9f1774483c93ddeabf62f5456c687d54d6b159c8
iuming <6ming@hrbeu.edu.cn>
2019-10-09 19:39:14 +0800

    iuming

---
+++ Problem one/Problem one program(Analytical Solution).cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#include<cstring>
+#include<map>
+#include<algorithm>
+#define N 51
+#define M 101
+using namespace std;
+int main()
+{
+    double L1,L2;
+    double t[M][N],T[M][N];
+    
+    return 0;
+}
