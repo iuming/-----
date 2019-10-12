7b948cd392da6f806d79aa70b59fb4dc5b202d7f
iuming <6ming@hrbeu.edu.cn>
2019-10-12 18:04:27 +0800

    iuming

--- Problem three/Problem three program(Numerical Solution).cpp
+++ Problem three/Problem three program(Numerical Solution).cpp
@@ -29,7 +29,7 @@ int main()
 	ofstream fout;  
 	fout.open("Problem three date(Numerical Solution).csv"); 
     double L1=20.0,L2=1.0,h=10,a=1000,m=0.5,n=0.5,tf=10,tw=50;
-    double esp=1.0;
+    double esp=1.0,Bi=h*L2/2/a;
 	double t[M][N],T[M][N];  
 	memset(t,0,sizeof(t));  
     memset(T,0,sizeof(T));  
@@ -38,11 +38,11 @@ int main()
             t[i][j]=30;
     for(int j=1;j<=N;j++)
         t[1][j]=50;
-    int k=0;
     while(esp>1e-6)
     {
-    	k++;
-    	int max1=0;
+    	for(int i=0;i<N;i++)
+   			for(int j=0;j<M;j++)
+    			T[i][j]=t[i][j];
     	for(int i=2;i<=M-1;i++)
     	{
     		t[i][N]=(n*a/(m*2)*(t[i+1][N]+t[i-1][N])+m*a/n*t[i][N-1]+h*tf*m)/(n*a/m+m*a/n+h*m);
@@ -52,20 +52,17 @@ int main()
 			t[M][N]=(n/2*a*t[M-1][N]/m+m/2*a*t[M][N-1]/n+m/2*h*tf+n/2*h*tf)/(n/2*a/m+m/2*a/n+m/2*h+n/2*h);
 			t[M][1]=(n/2*a/m*t[M-1][1]+m/2*a*t[M][2]/n+m/2*h*tf+n/2*h*tf)/(n/2*a/m+m/2*a/n+m/2*h+n/2*h);
 		}
-		for(int i=2;i<=M;i++)
-			for(int j=1;j<=N;j++)
-			{
-				esp=abs(t[i][j]-T[i][j]);
-				int max2=esp;
-				if(max2>=max1)max1=max2;
-			}
-		esp=max1;
+		esp=0;
+		for(int i=0;i<N;i++)
+   			for(int j=0;j<M;j++)
+    			esp+=abs(T[i][j]-t[i][j]);
+  		esp/=(N*M);
 		cout<<esp<<endl;
-		swap(t,T);
 	}
+	
     double Q=(t[2][1]-t[1][1])/m*n/2*a+(t[2][3]-t[1][3])/m*n/2*a+(t[2][2]-t[1][2])/m*n*a;
     cout<<Q<<endl;
-    for(int j=1;j<=N-1;j++)  
+    for(int j=0;j<=N-2;j++)  
     {  
        for(int i=1;i<=M-1;i++)  
             fout<<t[i][j]<<',';  
